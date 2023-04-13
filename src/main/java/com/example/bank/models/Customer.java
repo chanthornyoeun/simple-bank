bb9069c521039600f8bc2e7e58144233bcc0d44b
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2023-04-14 00:12:57 +0700

    Add customer model

---
+++ src/main/java/com/example/bank/models/Customer.java
@@ -0,0 +1,75 @@
+package com.example.bank.models;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    @Nonnull
+    String firstName;
+    @Nonnull
+    String lastName;
+    @Nonnull
+    String phone;
+    String email;
+    String address;
+
+    Customer() {
+
+    }
+
+    public Customer(String firstName, String lastName, String phone, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
